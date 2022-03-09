017c050323a88ca44577d88cc42d362c28a080ee
avh <andreas.vanhelden2@gmail.com>
2022-03-09 18:38:15 +0100

    add vuetify3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "vuetify/styles";
+import App from "./App.vue";
+import { loadFonts } from "./plugins/webfontloader";
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
-app.use(createPinia())
+loadFonts();
 
-app.mount('#app')
+createApp(App).use(vuetify).mount("#app");
