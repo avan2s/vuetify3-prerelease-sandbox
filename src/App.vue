ecd787cc59768ce9dca741d02c9309ae6963e75e
avh <andreas.vanhelden2@gmail.com>
2022-03-09 19:49:26 +0100

    add car table with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <h1>Lets drive a car</h1>
+      <v-container>
+        <v-navigation-drawer app>
+          <h1>Somesidebar</h1>
+        </v-navigation-drawer>
+        <v-select
+          :items="selectItems"
+          label="Brand"
+          v-model="carFilter.selectedBrand"
+          v-on:select="onElementClick('select')"
+        ></v-select>
+        <v-btn>Search</v-btn>
+
+        <v-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Brand</th>
+                <th class="text-center">Power (PS)</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in desserts"
+                :key="item.name"
+                v-on:click="onElementClick('row')"
+              >
+                <td class="text-left">{{ item.name }}</td>
+                <td class="text-left">{{ item.brand }}</td>
+                <td class="text-left">{{ item.power }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-table>
+      </v-container>
     </v-main>
   </v-app>
 </template>
@@ -19,8 +54,59 @@ export default defineComponent({
 
   data() {
     return {
-      //
+      selectItems: ["Audi", "BMW", "Ferrari", "Mercedes"],
+      desserts: [
+        {
+          name: "A4 Avant",
+          brand: "Audi",
+          power: 200,
+        },
+        {
+          name: "Q7 4M",
+          brand: "Audi",
+          power: 400,
+        },
+        {
+          name: "M140i",
+          brand: "BMW",
+          power: 272,
+        },
+        {
+          name: "M3e46",
+          brand: "BMW",
+          power: 305,
+        },
+        {
+          name: "F40",
+          brand: "Ferrari",
+          power: 600,
+        },
+        {
+          name: "Ferrari 512 BB",
+          brand: "Ferrari",
+          power: 500,
+        },
+        {
+          name: "Mercedes Benz A 180",
+          brand: "Mercedes",
+          power: 116,
+        },
+        {
+          name: "Mercedes-Benz E-Klasse Mercedes-AMG",
+          brand: "Mercedes",
+          power: 435,
+        },
+      ],
+      carFilter: {
+        selectedBrand: "Audi",
+      },
     };
+  },
+
+  methods: {
+    onElementClick(clickedElement) {
+      console.log("clicked on " + clickedElement);
+    },
   },
 });
 </script>
