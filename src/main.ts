96c7b6cee92cd3f02eacebda79427d0caff487f0
Andreas van Helden <andreas.vanhelden@conceto.de>
2022-03-10 19:10:47 +0100

    fix: mdi5 icons not rendered

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 import App from "./App.vue";
 import { loadFonts } from "./plugins/webfontloader";
