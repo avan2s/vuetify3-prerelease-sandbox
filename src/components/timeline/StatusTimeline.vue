d80703f4c288aec2fc5b589f40f9ab81c17354c0
avh <andreas.vanhelden2@gmail.com>
2022-03-12 15:00:22 +0100

    workaround for fixing timelines disappearing

--- src/components/timeline/StatusTimeline.vue
+++ src/components/timeline/StatusTimeline.vue
@@ -1,18 +1,24 @@
-import { ref } from 'vue';
 <template>
-  <h1>{{ title }}</h1>
-
-  <v-timeline single-side="after" :direction="direction" :density="'compact'">
-    <v-timeline-item
-      v-for="state in states"
-      :key="state.name"
-      align-dot="start"
-      :dot-color="state.color"
-    >
-      <strong>{{ state.name }}</strong>
-      <div class="text-caption mb-2">Hangouts</div>
-    </v-timeline-item>
-  </v-timeline>
+  <v-main>
+    <v-container fluid>
+      <h1>{{ title }}</h1>
+      <v-timeline
+        single-side="after"
+        :direction="direction"
+        :density="'compact'"
+      >
+        <v-timeline-item
+          v-for="state in states"
+          :key="state.name"
+          align-dot="start"
+          :dot-color="state.color"
+        >
+          <strong>{{ state.name }}</strong>
+          <div class="text-caption mb-2">Hangouts</div>
+        </v-timeline-item>
+      </v-timeline>
+    </v-container>
+  </v-main>
 </template>
 <script setup lang="ts">
 defineProps<{
